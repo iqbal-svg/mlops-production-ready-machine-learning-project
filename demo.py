7c192a4fd76721dca3d8fb3625084ae0736be4f8
Iqbal Shaikh <iqbalshaikhsap@gmail.com>
2025-08-11 12:29:28 +0530

    logging module demo

--- demo.py
+++ demo.py
@@ -1,5 +1,7 @@
-from us_visa.pipline.training_pipeline import TrainPipeline  # Import the TrainPipeline class from the 'training_pipeline' module in the 'us_visa/pipline' package. This class is designed to manage the complete ML training process.
+# from us_visa.pipline.training_pipeline import TrainPipeline  # Import the TrainPipeline class from the 'training_pipeline' module in the 'us_visa/pipline' package. This class is designed to manage the complete ML training process.
 
-obj = TrainPipeline()  # Create an instance of the TrainPipeline class so we can call its methods.
+# obj = TrainPipeline()  # Create an instance of the TrainPipeline class so we can call its methods.
 
-obj.run_pipeline()  # Call the 'run_pipeline' method to start the end-to-end ML pipeline (data loading, preprocessing, model training, evaluation, saving results, etc.).
+# obj.run_pipeline()  # Call the 'run_pipeline' method to start the end-to-end ML pipeline (data loading, preprocessing, model training, evaluation, saving results, etc.).
+from us_visa.logger import logging
+logging.info("welcome to our custom log")
