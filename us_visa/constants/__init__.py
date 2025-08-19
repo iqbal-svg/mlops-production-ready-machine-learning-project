9593af704b836a1ff58def26981dda95fb84dee6
Iqbal Shaikh <iqbalshaikhsap@gmail.com>
2025-08-19 16:28:25 +0530

    modular coding

---
+++ us_visa/constants/__init__.py
@@ -0,0 +1,16 @@
+import os
+from datetime import date
+
+DATABASE_NAME = "us_visa"
+COLLECTION_NAME = "visa_data"
+MONGODB_URL_KEY = "MONGODB_URL"
+
+PIPELINE_NAME: str = "usvisa"
+ARTIFACT_DIR: str = "artifact"
+
+
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+
+FILE_NAME: str = "usvisa.csv"
+MODEL_FILE_NAME = "model.pkl"
