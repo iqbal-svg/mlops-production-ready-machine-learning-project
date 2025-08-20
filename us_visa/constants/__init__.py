144d9ae2ca919efb423524d6f00d0fa3395058aa
Iqbal Shaikh <iqbalshaikhsap@gmail.com>
2025-08-20 14:58:54 +0530

    modular

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -14,3 +14,19 @@ TEST_FILE_NAME: str = "test.csv"
 
 FILE_NAME: str = "usvisa.csv"
 MODEL_FILE_NAME = "model.pkl"
+
+#Data Ingestion related constant start with DATA_INGESTION VAR NAME
+# MongoDB collection name where raw data is stored
+DATA_INGESTION_COLLECTION_NAME: str = "visa_data"  
+
+# Main directory for data ingestion artifacts
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"  
+
+# Sub-directory to store raw data (feature store = permanent storage of original features)
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"  
+
+# Sub-directory to store train/test split data after ingestion
+DATA_INGESTION_INGESTED_DIR: str = "ingested"  
+
+# Ratio for splitting data into test set (e.g., 0.2 means 20% test, 80% train)
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
